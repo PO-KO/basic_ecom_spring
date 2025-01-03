53e473cbb1228c184afbf6f7a48ebaa8f1c7b337
dano <ayoub18daoui@gmail.com>
2025-01-03 14:45:03 +0100

    first commit

---
+++ src/main/java/com/dano/basic_ecom/BasicEcomApplication.java
@@ -0,0 +1,13 @@
+package com.dano.basic_ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicEcomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dano/basic_ecom/BasicEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dano.basic_ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicEcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
